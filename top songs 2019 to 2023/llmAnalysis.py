3d53c61b67b95a9d9a4af106971e8d53f24a571b
yshukla0 <150735799+yshukla0@users.noreply.github.com>
2024-08-22 01:47:25 -0400

    processed some songs through gemini for testing

--- top songs 2019 to 2023/llmAnalysis.py
+++ top songs 2019 to 2023/llmAnalysis.py
@@ -1,13 +1,44 @@
 
 import google.generativeai as genai
 import os
+import csv
+import time
 
 genai.configure(api_key='AIzaSyAAceK2tD3AZAO2UUzIY4R5AD3K-zKd1-4')
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-entity = 'Rari'
-sentence = "Crashed the 'Rari so I hopped in the Benz"
+songWordsFile = 'song words data/songWords.csv'
+sentimentOutputFile = 'song words data/sentimentAnalysis.csv'
 
-response = model.generate_content(f"What is the sentiment (positive/negative/neutral) of the {entity} in {sentence} in one word")
-print(response.text)
+def readSongWordsFile(fileName):
+    with open(fileName, 'r') as file:
+        csvReader = csv.reader(file)
+        rows = list(csvReader)[1:]  
+    return rows
+
+def analyzeEntitiesSentiment():
+    songData = readSongWordsFile(songWordsFile)
+
+    with open(sentimentOutputFile, 'w', newline='') as file:
+        csvWriter = csv.writer(file)
+        csvWriter.writerow(['Title', 'Artist', 'Year', 'Entity','Entity Type', 'Sentiment'])
+        
+        for row in songData:
+            print(row)
+            title = row[0]
+            artist = row[1]
+            year = row[2]
+            entityLines = eval(row[4])
+            for entityType in ['brands', 'locations']:
+                for entity, line in entityLines[entityType].items():
+                    sentiment = analyzeSentiment(entity, line)
+                    csvWriter.writerow([title, artist, year, entity, entityType, sentiment, line])
+
+def analyzeSentiment(entity, text):
+    response = model.generate_content(f"What is the sentiment (positive/negative/neutral) of the {entity} in {text} in one word. If there is not enough information, make a guess. Your answer should never be more than one word or anything other than the 3 options provided. If the statement is not appropriate, skip the evaluation, return 'skip'.")
+    time.sleep(2)
+    return response.text
+
+analyzeEntitiesSentiment()
+
